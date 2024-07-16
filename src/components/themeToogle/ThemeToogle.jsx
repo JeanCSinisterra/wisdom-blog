4bbe261b086df098ea29bb5623d3a73bad595dda
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2024-07-16 06:52:40 -0500

    Theme components on build

--- src/components/themeToogle/ThemeToogle.jsx
+++ src/components/themeToogle/ThemeToogle.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import styles from "./ThemeToogle.module.css"
+"use client";
+
+import React, { useContext } from 'react'
+import styles from "./themeToogle.module.css"
 import Image from 'next/image'
+import { ThemeContext } from '@/context/ThemeContext'
 
 const ThemeToogle = () => {
+  const {theme} = useContext(ThemeContext)
+  console.log(theme);
+
   return (
     <div className={styles.container}>
       <Image src="/moon.png" alt="Dark Theme" width={14} height={14} />
