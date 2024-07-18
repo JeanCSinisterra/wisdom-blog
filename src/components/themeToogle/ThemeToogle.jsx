ccd6af8e9be1f2705c12ce458d8b47c812c23e27
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2024-07-17 21:12:36 -0500

    Dark Theme Toggle fixed

--- src/components/themeToogle/ThemeToogle.jsx
+++ src/components/themeToogle/ThemeToogle.jsx
@@ -6,11 +6,11 @@ import Image from 'next/image'
 import { ThemeContext } from '@/context/ThemeContext'
 
 const ThemeToogle = () => {
-  const {theme} = useContext(ThemeContext)
+  const {theme ,toggle} = useContext(ThemeContext)
   console.log(theme);
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <Image src="/moon.png" alt="Dark Theme" width={14} height={14} />
       <div className={styles.ball}></div>
       <Image src="/sun.png" alt="Light Theme" width={14} height={14} />
