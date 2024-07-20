c6c5ff2f507ac652defa3cce7fdcb6f6ca603b2a
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2024-07-20 16:45:24 -0500

    Blog card compononent finished

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "./card.module.css"
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Card = () => {
     return (
@@ -10,9 +11,14 @@ const Card = () => {
             </div>
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>12.07.2024</span>
+                    <span className={styles.date}>12.07.2024 - </span>
                     <span className={styles.category}>CULTURE</span>
                 </div>
+                <Link href="/">
+                <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+                </Link>
+                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laudantium sed, doloremque...</p>
+                <Link href="/" className={styles.link}>Read More</Link>
             </div>
         </div>
     )
